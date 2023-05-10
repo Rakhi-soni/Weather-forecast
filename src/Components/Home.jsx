a89d9d652a7237ea8b304db5005f283598987cc8
rakhi <rakhisoni908@gmail.com>
2023-05-10 22:35:26 +0530

    api fetch

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,14 +6,33 @@ import Input from './Input.jsx';
 
 
 function Home() {
+  let currentDate = new Date();
+  let date= currentDate.getDate() + "-" + (currentDate.getMonth() +1)+ "-" +currentDate.getFullYear()
+  console.log( currentDate.getDate() + "-" + (currentDate.getMonth() +1)+ "-" +currentDate.getFullYear() );
+
+  
+  let currentTime = new Date();
+ let time = currentTime.getHours() + ":" + currentTime.getMinutes();
+ console.log(time);
+  
+//   function time(){
+//   var d = new Date();
+  
+//   var m = d.getMinutes();
+//   var h = d.getHours();
+//   var c= h + ":" + m;
+//   document.getElementById('').innerHTML(c);
+// }
+// setInterval(time,1000);
+
   return (
     <>
       <div className='main'>
         <div className='l-part'>
           <div className='datentime'>
-            <span>1 May 2023</span>
+            <span>{date}</span>
             <span>|</span>
-            <span>21:00</span>
+            <span>{time}</span>
           </div>
           <div className='text'>Heavy Rain</div><br />
           <div className='divider'></div>
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {FaCloudRain,FaCloudSun,FaCloudMoon,FaSearch} from 'react-icons/fa';
 import './Input.css';
 import Weather from './Weather';
+import axios from 'axios';
 
 
 function Input() {
+
+const [city,setCity] = useState(null);
+const [search,setSearch] = useState("bangalore");
+ 
+
+useEffect(()=>{
+ const weather=()=>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=54c6df5b50d105214b8ccac0b8e8006c`)
+    .then((response)=>{
+        console.log(response);
+        setCity(response);
+    }).catch((reject)=>{
+        console.log("error");
+    })
+ };
+ weather();
+},[search])
+
     return (
 <div className='I-main'>
-    <div className='input'><span><FaSearch className='search'/></span><input type='search' placeholder='Enter Your City Name'/></div>
-    <div className='result'>11&deg;C</div>
-    <div className='I-text'>Northwest, 38.9 km/h</div>
+    <div className='input'><span><FaSearch className='search'/></span><input type='search' className='inputdata' onChange={(event)=>{
+     setSearch(event.target.value)}} placeholder='Enter Your City Name'/></div>
+     {!city?(
+        <p>no data found</p>
+     ):(
+        <>
+<div><h2>{search}</h2></div>
+    <div className='result'>{city.data.main.temp}&deg;C</div>
+    <div className='I-text'>{city.data.weather[0].main}</div>
+     
     <div className='I-divider'></div>
     <div className='I-forecast'>The Next Day Forecast</div>
     <div className='I-days'>
@@ -18,6 +44,9 @@ function Input() {
         <div className='days'>30days</div>
     </div>
     <div className='I-weather'><Weather/></div>
+    </>
+     )}
+    
 </div>
         );
     }
